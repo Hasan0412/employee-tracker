7b36a94b3e990adce12a6a9448ec011b30b3c3c4
Hasan Malik <hmalik0412@gmail.com>
2023-09-05 03:43:17 +0100

    error fixing

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,25 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department
-{
-    id INT NOT NULL
-    AUTO_INCREMENT PRIMARY KEY,
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30)
-};
+);
 
 CREATE TABLE role
-{
-    id INT NOT NULL
-    AUTO_INCREMENT PRIMARY KEY,
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department (id)
     ON DELETE SET NULL
-};
+);
 
 CREATE TABLE employee
-{
-    id INT NOT NULL
-    AUTO_INCREMENT PRIMARY KEY,
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT,
@@ -33,4 +30,4 @@ CREATE TABLE employee
     FOREIGN KEY (role_id)
     REFERENCES role (id)
     ON DELETE SET NULL
-};
+);
