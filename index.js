5f083e3ace93c82b013bc85b6700c891c3f1e0c2
Hasan Malik <hmalik0412@gmail.com>
2023-09-05 02:38:08 +0100

    Added comments to describe functionality index.js

--- index.js
+++ index.js
@@ -27,71 +27,99 @@ async function app() {
 
         switch (action) {
             case 'Show All Departments':
+                // Retrieve and display all departments from the database
                 const departments = await database.viewDepartments();
                 console.table(departments);
                 break;
             case 'Show All Roles':
+                // Retrieve and display all roles from the database
                 const roles = await database.viewRoles();
                 console.table(roles);
                 break;
             case 'Show All Employees':
+                // Retrieve and display all employees from the database
                 const employees = await database.viewEmployees();
                 console.table(employees);
                 break;
             case 'Add a Department':
+                // Prompt the user to enter a new department name
                 const {name} = await inquirer.prompt(
                     {type: 'input', name: 'title', message: 'Enter the new Department name:'}
                 );
+                // Add the new department to the database
                 await database.additionalDepartment(name);
-                break; 
+                break;
             case 'Add a Role':
+                // Prompt the user to enter a new role's name and salary
                 const newRole = await inquirer.prompt([
                     {type: 'input', name: 'title', message: 'Enter the new Role name:'},
                     {type: 'input', name: 'salary', message: 'Enter the Salary for this new Role:'},
                 ]);
+                // Prompt the user to select a department for the new role
                 const department_id = await promptDepartmentChoice();
+                // Add the new role to the database
                 await database.additionalRole(newRole.title, newRole.salary, department_id);
-                break;              
+                break;
             case 'Add an Employee':
+                // Prompt the user to enter a new employee's first name and last name
                 const newEmployee = await inquirer.prompt([
                     { type: 'input', name: 'first_name', message: 'Add the first name:' },
                     { type: 'input', name: 'last_name', message: 'Add the last name:' },
                 ]);
+                // Prompt the user to select a role for the new employee
                 const employee_role_id = await promptRoleChoice();
+                // Prompt the user to select a manager for the new employee
                 const manager_id = await promptEmployeeChoice('Select Manager for new Employee:');
+                // Add the new employee to the database
                 await database.additionalEmployee(newEmployee.first_name, newEmployee.last_name, employee_role_id, manager_id);
+                break;
             case 'Change an Employee Role':
+                // Prompt the user to select an employee to update
                 const employee_to_update_id = await promptEmployeeChoice('Select an Employee:');
+                // Prompt the user to select a new role for the employee
                 const new_employee_role_id = await promptRoleChoice();
+                // Update the employee's role in the database
                 await database.changeEmployeeRole(new_employee_role_id, employee_to_update_id);
                 break;
-            case 'Change an Employees Manager':
+            case 'Change an Employee\'s Manager':
+                // Prompt the user to select an employee to update
                 const employee_to_update_manager_id = await promptEmployeeChoice('Select an employee:');
+                // Prompt the user to select a new manager for the employee
                 const manager_to_update_id = await promptEmployeeChoice('Select a new Manager for this employee:');
+                // Update the employee's manager in the database
                 await database.changeEmployeeManager(employee_to_update_manager_id, manager_to_update_id);
                 break;
             case 'Remove a Department':
+                // Prompt the user to select a department to delete
                 const department_to_delete_id = await promptDepartmentChoice();
+                // Remove the selected department from the database
                 await database.removeDepartment(department_to_delete_id);
                 break;
             case 'Remove a Role':
+                // Prompt the user to select a role to delete
                 const role_to_delete_id = await promptRoleChoice();
+                // Remove the selected role from the database
                 await database.removeRole(role_to_delete_id);
                 break;
             case 'Remove an Employee':
+                // Prompt the user to select an employee to remove
                 const employee_to_delete_id = await promptEmployeeChoice('Select an employee to remove:');
+                // Remove the selected employee from the database
                 await database.removeEmployee(employee_to_delete_id);
                 break;
+                // Exit the application if the user selects 'Quit Application'
             case 'Quit Application':
                 continueExecution = false;
                 break;
         }
     }
 
+    // Exit the Node.js application
     process.exit();
 
 }
 
+// Helper function to prompt the user to select a department
 async function promptDepartmentChoice() {
     const allDepartments = await database.viewDepartments();
     const departmentOptions = allDepartments.map((department) => ({
@@ -109,6 +137,7 @@ async function promptDepartmentChoice() {
     return department_id;
 }
 
+// Helper function to prompt the user to select a role
 async function promptRoleChoice() {
     const allRoles = await database.viewRoles();
     const roleOptions = allRoles.map((role) => ({
@@ -126,6 +155,7 @@ async function promptRoleChoice() {
     return role_id;
 }
 
+// Helper function to prompt the user to select an employee
 async function promptEmployeeChoice(message) {
     const allEmployees = await database.viewEmployees();
     const employeeOptions =
@@ -147,4 +177,5 @@ async function promptEmployeeChoice(message) {
     return employee_id;
 }
 
+// Start the main application
 app();
